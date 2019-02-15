8650cec4564f5f30e12e8012e41943097f818165
wisdom.emenike <wisdom.emenike@imaxinacion.info>
2019-02-15 12:50:24 +0100

    Refactored code base

--- vojax.js
+++ vojax.js
@@ -5,27 +5,27 @@ NetBridge = (function() {
         let permitNetwork = true;
         let getPermitNetwork = () => permitNetwork;
         let setPermitNetwork = (status) => {
-            if (is_boolean(status)) permitNetwork = status;
+            if (isBoolean(status)) permitNetwork = status;
         };
 
-        let is_undefined = (variable) => typeof variable === "undefined";
-        let is_object = (variable) => typeof variable === "object";
-        let is_function = (variable) => typeof variable === "function";
-        let is_boolean = (variable) => typeof variable === "boolean";
-        let is_string = (variable) => typeof variable === "string";
-        let is_numeric = (variable) => isNaN(variable) === false;
-        let is_empty = (variable) => variable === false || variable === null ||
+        let isUndefined = (variable) => typeof variable === "undefined";
+        let isObject = (variable) => typeof variable === "object";
+        let isFunction = (variable) => typeof variable === "function";
+        let isBoolean = (variable) => typeof variable === "boolean";
+        let isString = (variable) => typeof variable === "string";
+        let isNumeric = (variable) => isNaN(variable) === false;
+        let isEmpty = (variable) => variable === false || variable === null ||
             variable.toString() === "0" || variable.toString() === "" || variable.toString() === " ";
-        let get_type = (variable) => typeof variable;
+        let getType = (variable) => typeof variable;
         let serialize = (object) => {
             let serial = "", x;
             for (x in object) {
                 if (object.hasOwnProperty(x))
-                    serial += (!is_empty(serial) ? "&" : "") + x + "=" + object[x];
+                    serial += (!isEmpty(serial) ? "&" : "") + x + "=" + object[x];
             }
             return serial;
         };
-        let in_request_queue = (request) => {
+        let isInRequestQueue = (request) => {
             let requestQueue = this.getRequestQueue(),
                 size = requestQueue.length;
             if (size <= 0) return false;
@@ -49,13 +49,13 @@ NetBridge = (function() {
         this.addToRequestQueue = (queue) => {
 
             let size = this.getRequestQueue().length, network = getPermitNetwork();
-            if (!is_object(queue)) throw "vojax expects an object from its parameter, but got " + get_type(queue);
-            if (is_undefined(queue.url)) throw "vojax expects a 'url' attribute from the passed object";
-            if (!is_string(queue.url)) throw "vojax expects the 'url' attribute to be a string, but got " + get_type(queue.url);
-            if (is_undefined(queue.method)) throw "vojax expects a 'method' attribute from the passed object";
-            if (!is_string(queue.method)) throw "vojax expects the 'method' attribute to be a string, but got " + get_type(queue.method);
-            if (in_request_queue(queue)) return;
-            if (!network && is_function(queue.queue)) queue.queue();
+            if (!isObject(queue)) throw "NetBridge expects an object from its parameter, but got " + getType(queue);
+            if (isUndefined(queue.url)) throw "NetBridge expects a 'url' attribute from the passed object";
+            if (!isString(queue.url)) throw "NetBridge expects the 'url' attribute to be a string, but got " + getType(queue.url);
+            if (isUndefined(queue.method)) throw "NetBridge expects a 'method' attribute from the passed object";
+            if (!isString(queue.method)) throw "NetBridge expects the 'method' attribute to be a string, but got " + getType(queue.method);
+            if (isInRequestQueue(queue)) return;
+            if (!network && isFunction(queue.queue)) queue.queue();
             push(queue);
             if (size <= 0) sendRequest();
 
@@ -85,7 +85,7 @@ NetBridge = (function() {
                     let xhttp = new XMLHttpRequest();
 
                     // noinspection Annotator
-                    if (is_function(request['beforeSend'])) {
+                    if (isFunction(request['beforeSend'])) {
                         xhttp.onloadstart = () => {
                             // noinspection Annotator
                             request['beforeSend'](xhttp);
@@ -98,36 +98,36 @@ NetBridge = (function() {
                             status = false;
 
                         if (this.readyState === 0) {
-                            console.error("vojax error: request not initialized (URL:: " + request.url + ")");
-                            if (is_function(request.error)) request.error(xhttp, this.status, this.statusText);
+                            console.error("NetBridge error: request not initialized (URL:: " + request.url + ")");
+                            if (isFunction(request.error)) request.error(xhttp, this.status, this.statusText);
                         }
 
                         if (this.readyState === 4) state = true;
 
                         if (this.status === 403) {
-                            console.error("vojax error: request forbidden (URL:: " + request.url + ")");
-                            if (is_function(request.error)) request.error(xhttp, this.status, this.statusText);
+                            console.error("NetBridge error: request forbidden (URL:: " + request.url + ")");
+                            if (isFunction(request.error)) request.error(xhttp, this.status, this.statusText);
                         }
 
                         if (this.status === 404) {
-                            console.error("vojax error: not found (URL:: " + request.url + ")");
-                            if (is_function(request.error)) request.error(xhttp, this.status, this.statusText);
+                            console.error("NetBridge error: not found (URL:: " + request.url + ")");
+                            if (isFunction(request.error)) request.error(xhttp, this.status, this.statusText);
                         }
 
                         if (this.status === 200) status = true;
 
                         if (state === true && status === true) {
-                            if (is_function(request.success)) request.success(this.responseText, this.status, xhttp);
+                            if (isFunction(request.success)) request.success(this.responseText, this.status, xhttp);
                         }
 
-                        if (is_function(request.complete)) request.complete(xhttp, this.status);
+                        if (isFunction(request.complete)) request.complete(xhttp, this.status);
 
                     };
 
                     xhttp.onloadend = () => {
                         setPermitNetwork(true);
                         // noinspection Annotator
-                        if (is_boolean(request['persist']) && request['persist'] === true) push(request);
+                        if (isBoolean(request['persist']) && request['persist'] === true) push(request);
                         let _tm = setTimeout(() => {
                             let queue = this.getRequestQueue();
                             if (queue.length > 0) send(shift());
@@ -135,41 +135,41 @@ NetBridge = (function() {
                         }, 100);
                     };
 
-                    xhttp.msCaching = (is_boolean(request.cache) ? request.cache : false);
+                    xhttp.msCaching = (isBoolean(request.cache) ? request.cache : false);
 
                     // noinspection Annotator
                     xhttp.open(
                         request.method,
                         request.url,
-                        (is_boolean(request.async) ? request.async : true),
-                        (is_string(request['username']) ? request['username'] : ""),
-                        (is_string(request.password) ? request.password : ""),
+                        (isBoolean(request.async) ? request.async : true),
+                        (isString(request['username']) ? request['username'] : ""),
+                        (isString(request.password) ? request.password : ""),
                     );
 
-                    if (is_function(request.xhr)) request.xhr();
+                    if (isFunction(request.xhr)) request.xhr();
 
                     xhttp.setRequestHeader("Content-type", (
-                        (is_boolean(request.contentType) && request.contentType !== false ?
+                        (isBoolean(request.contentType) && request.contentType !== false ?
                                 "application/x-www-form-urlencoded" : (
-                                    is_string(request.contentType) ?
+                                    isString(request.contentType) ?
                                         request.contentType :
                                         "application/x-www-form-urlencoded"
                                 )
                         )
                     ));
 
-                    if (is_object(request.headers)) {
+                    if (isObject(request.headers)) {
                         for (let x in request.headers) {
                             if (request.headers.hasOwnProperty(x))
                                 xhttp.setRequestHeader(x, request.headers[x]);
                         }
                     }
 
-                    if (is_numeric(request.timeout)) xhttp.timeout = parseInt(request.timeout);
+                    if (isNumeric(request.timeout)) xhttp.timeout = parseInt(request.timeout);
 
-                    if (is_function(request.ontimeout)) xhttp.ontimeout = request.ontimeout;
+                    if (isFunction(request.ontimeout)) xhttp.ontimeout = request.ontimeout;
 
-                    xhttp.send((is_boolean(request.processData) && request.processData === false ? request.data : serialize(request.data)));
+                    xhttp.send((isBoolean(request.processData) && request.processData === false ? request.data : serialize(request.data)));
 
                 };
 
